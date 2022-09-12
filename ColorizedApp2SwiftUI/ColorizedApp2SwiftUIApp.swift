94c20dcf4dfd664095be35af0417fa2e4ba95e8a
Oksenoyt <a.oksenoyt@gmail.com>
2022-09-12 19:03:01 +0400

    Initial Commit

---
+++ ColorizedApp2SwiftUI/ColorizedApp2SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp2SwiftUIApp.swift
+//  ColorizedApp2SwiftUI
+//
+//  Created by Elenka on 12.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp2SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
